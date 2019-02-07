f148a8ad4d3cafe12080d85058a6f1563d56097a
Jun Kelly <kellyja4@gmail.com>
2019-02-07 11:57:14 -0800

    css module webpack and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react']
         }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
       }
     ]
   },
