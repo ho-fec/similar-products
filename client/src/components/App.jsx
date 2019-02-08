8d25636002fc91f24d0b462308fa8ce2f6e60bd9
Jun Kelly <kellyja4@gmail.com>
2019-02-07 20:11:50 -0800

    Created App container

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Carousel from './Carousel.jsx';
+import styles from '../css/App.css';
 
 const App = () => (
-  <div>Hello from App</div>
+  <div className={ styles.box }>
+    <div className={ styles.title } >Similar Products</div>
+    <div className={ styles.box }>
+      <Carousel />
+    </div>
+    {/* <Select /> */}
+  </div>
 );
 
 export default App;
