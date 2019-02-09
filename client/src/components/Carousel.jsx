12f6ef8993be8b0b4c8b53495eaad092092e32aa
Jun Kelly <kellyja4@gmail.com>
2019-02-08 16:04:42 -0800

    Imported images from database and rendered onto carousel

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -3,18 +3,27 @@ import CarouselItem from './CarouselItem.jsx';
 import styles from '../css/Carousel.css';
 import lArrow from '../assets/left-arrow.svg';
 import rArrow from '../assets/right-arrow.svg';
-const faker = require('faker');
+import axios from 'axios';
 
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state ={
-      list: ['https://sephora.com/productimages/sku/s1897487-main-grid.jpg',
-      'https://sephora.com/productimages/sku/s1897495-main-grid.jpg',
-      'https://sephora.com/productimages/sku/s487694-main-grid.jpg',
-      'https://sephora.com/productimages/sku/s1217710-main-grid.jpg',
-      'https://sephora.com/productimages/sku/s1835420-main-grid.jpg']
+      list: []
     }
+  }
+
+  // 'https://sephora.com/productimages/sku/s1897487-main-grid.jpg',
+  //     'https://sephora.com/productimages/sku/s1897495-main-grid.jpg',
+  //     'https://sephora.com/productimages/sku/s487694-main-grid.jpg',
+  //     'https://sephora.com/productimages/sku/s1217710-main-grid.jpg',
+  //     'https://sephora.com/productimages/sku/s1835420-main-grid.jpg'
+
+  componentDidMount() {
+    axios
+      .get('/item')
+      .then(({ data }) => this.setState({ list: [...data] }))
+      .catch(err => console.log(err))
   }
 
   render() {
@@ -24,7 +33,7 @@ class Carousel extends React.Component {
           <img className={ styles.arrow } src={lArrow} />
         </button>
         {this.state.list.map((item, i) =>
-        <CarouselItem key={i} item={item} />)}
+        <CarouselItem key={i} item={item.image} />)}
         <button className={ styles.button }>
           <img className={ styles.arrow } src={rArrow} />
         </button>
