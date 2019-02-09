66eaefed45c2fc65aca74a1fe2b9159090342b46
Jun Kelly <kellyja4@gmail.com>
2019-02-09 14:29:26 -0800

    Rearranged arrows and containers to not overlap, began work on scrolling function

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -10,8 +10,10 @@ class Carousel extends React.Component {
     super(props);
     this.state = {
       list: [],
-      position: 0
+      move: false
     }
+    this.clickLeft = this.clickLeft.bind(this);
+    this.clickRight = this.clickRight.bind(this);
   }
 
   componentDidMount() {
@@ -20,25 +22,41 @@ class Carousel extends React.Component {
       .then(({ data }) => this.setState({ list: [...data] }))
       .catch(err => console.log(err))
   }
+  
+  clickLeft(e) {
+    this.setState({ move: true });
+  }
+
+  clickRight(e) {
+    this.setState({ move: true });
+  }
 
   render() {
+    let { move } = this.state;
+
     return (
-      <div className={ styles.wrapper }>
-        <button className={ styles.button }>
-          <img className={ styles.arrow } src={ lArrow } />
-        </button>
-        <div className={ styles.innerContainer }>
-          {this.state.list.map((item, i) =>
-          <CarouselItem
-            key={ i }
-            item={ item.image }
-            name={ item.product_name }
-            category={ item.category }
-            price={ item.price } />)}
+      <div className={ styles.outerWrapper }>
+        <div className={ styles.innerWrapper }>
+          <div className={ styles.outer }>
+            <div className={ styles.inner }>
+              {this.state.list.map((item, i) =>
+              <CarouselItem
+                key={ i }
+                item={ item.image }
+                name={ item.product_name }
+                category={ item.category }
+                price={ item.price } />)}
+            </div>
+          </div>
+
+          <button className={ styles.buttonL } onClick={ this.clickRight } >
+            <img className={ styles.arrow } src={ lArrow } />
+          </button>
+
+          <button className={ styles.buttonR } onClick={ this.clickRight } >
+            <img className={ styles.arrow } src={ rArrow } />
+          </button>
         </div>
-        <button className={ styles.button }>
-          <img className={ styles.arrow } src={ rArrow } />
-        </button>
       </div>
     )
   }
