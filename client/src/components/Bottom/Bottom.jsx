06ef2aa02147bc74e706b0699e38a5e3bf343388
Jun Kelly <kellyja4@gmail.com>
2019-02-12 16:53:26 -0800

    Refactored Bottom.jsx into stateless and tweaked button styling

--- client/src/components/Bottom/Bottom.jsx
+++ client/src/components/Bottom/Bottom.jsx
@@ -1,38 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './Bottom.css';
 
-class Bottom extends Component {
-  constructor(props) {
-    super(props);
-  }
+export default (props) => (
+  <div className={ styles.bottom }>
 
-  render() {
-    return (
-      <div className={ styles.bottom }>
-        <button
-        type='button'
-        className={ styles.slideButton }
-        onClick={ () => this.props.click(0) }
-        disabled={ this.props.dL }>
-          <div className={ styles.circle }></div>
-        </button>
-        <button
-        type='button'
-        className={ styles.slideButton }
-        onClick={ () => this.props.click(1) }
-        disabled={ this.props.dM }>
-          <div className={ styles.circle }></div>
-        </button>
-        <button
-        type='button'
-        className={ styles.slideButton }
-        onClick={ () => this.props.click(2) }
-        disabled={ this.props.dR }>
-          <div className={ styles.circle }></div>
-        </button>
-      </div>
-    );
-  }
-}
+    <button
+    type='button'
+    className={ styles.slideButton }
+    onClick={ () => props.click(0) }
+    disabled={ props.dL }>
+      <div className={ styles.circle }></div>
+    </button>
 
-export default Bottom;
+    <button
+    type='button'
+    className={ styles.slideButton }
+    onClick={ () => props.click(1) }
+    disabled={ props.dM }>
+      <div className={ styles.circle }></div>
+    </button>
+
+    <button
+    type='button'
+    className={ styles.slideButton }
+    onClick={ () => props.click(2) }
+    disabled={ props.dR }>
+      <div className={ styles.circle }></div>
+    </button>
+
+  </div>
+);
