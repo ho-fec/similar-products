8fd11afb494cc6ccc1528f24fd7f1a40ce40514e
Jun Kelly <kellyja4@gmail.com>
2019-02-14 13:59:20 -0800

    Refactored into stateful component to conditionally render buttons for additional sizes

--- client/src/components/Modal/ModalInfo/ModalInfo.jsx
+++ client/src/components/Modal/ModalInfo/ModalInfo.jsx
@@ -1,39 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './ModalInfo.css';
 
-function ModalInfo(props) {
-  const oz = (ml) => {
-    return (ml * 0.033814).toFixed(1);
+class ModalInfo extends Component {
+  constructor(props) {
+    super(props);
   }
-  return (
-    <div className={ styles.productInfo }>
-      <div className={ styles.productNameContainer }>
-      <span className={ styles.category }>{ props.category }</span>
-      <span className={ styles.name }>{ props.name }</span>
-      </div>
-      <div className={ styles.sizeSKU }>
-        <span>
-          SIZE { oz(props.size) } oz/ { props.size }mL
-          <span className={ styles.bullet }>•</span>
-          </span>
-          ITEM {props.sku}
-      </div>
-      <div className={ styles.description }>
-        <div className={ styles.descriptionText }>
-          { props.description }
+
+  render() {
+    const oz = (ml) => {
+      return (ml * 0.033814).toFixed(1);
+    }
+
+    return (
+      <div className={ styles.productInfo }>
+        <div className={ styles.productNameContainer }>
+        <span className={ styles.category }>{ this.props.category }</span>
+        <span className={ styles.name }>{ this.props.name }</span>
         </div>
-        <div className={ styles.productLinkContainer }>
-          <span className={ styles.productLink }>
-            See product details
-          </span>
+        <div className={ styles.sizeSKU }>
+          <span>
+            SIZE { oz(this.props.size) } oz/ { this.props.size }mL
+            <span className={ styles.bullet }>•</span>
+            </span>
+            ITEM { this.props.sku }
+        </div>
+        <div className={ styles.description }>
+          <div className={ styles.descriptionText }>
+            { this.props.description }
+          </div>
+          <div className={ styles.productLinkContainer }>
+            <span className={ styles.productLink }>
+              See product details
+            </span>
+          </div>
+        </div>
+        <div className={ styles.variationText }>
+        </div>
+        <div className={ styles.variationBox }>
         </div>
       </div>
-      <div className={ styles.variationText }>
-      </div>
-      <div className={ styles.variationBox }>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ModalInfo;
