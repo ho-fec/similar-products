f9ed7ca2dbcbfd3b64f4c338b24a91ac7f0e9cce
Jun Kelly <kellyja4@gmail.com>
2019-02-15 23:30:20 -0800

    Put Love icon into component

---
+++ client/src/components/Modal/ModalBottomLeft/ModalLove.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+export default ({ className='' }) => (
+  <svg className={ className } viewBox={ '0 0 32 26' } xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+  <path d="M16.003 26c-.915 0-1.772-.354-2.417-1L2.364 13.78C.84 12.254 0 10.228 0 8.07 0 3.078 4.153-.012 8-.012c2.225 0 4.223.822 5.778 2.377L16 4.586l2.222-2.222C19.777.81 21.775-.013 24-.013c3.848 0 8 3.09 8 8.084 0 2.157-.84 4.184-2.364 5.708L18.413 25c-.643.645-1.5 1-2.41 1z"></path>
+  </svg>
+);
