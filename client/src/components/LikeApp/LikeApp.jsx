2782838fa1fc50d5388d47f6a967ee580ae705b5
Jun Kelly <kellyja4@gmail.com>
2019-02-23 11:01:13 -0800

    Added borders

--- client/src/components/LikeApp/LikeApp.jsx
+++ client/src/components/LikeApp/LikeApp.jsx
@@ -3,14 +3,18 @@ import Carousel2 from '../Carousel2';
 import styles from './LikeApp.css';
 
 const LikeApp = props => (
-  <div className={ styles.box }>
-    <div className={ styles.titleWrapper }>
-      <div className={ styles.title }>
-        You May Also Like
+  <>
+    <div className={ styles.divider }></div>
+    <div className={ styles.box }>
+      <div className={ styles.titleWrapper }>
+        <div className={ styles.title }>
+          You May Also Like
+        </div>
       </div>
+      <Carousel2 id={ props.id }/>
     </div>
-    <Carousel2 id={ props.id }/>
-  </div>
+    <div className={ styles.divider }></div>
+  </>
 );
 
 export default LikeApp;
--- client/src/components/SimilarApp/SimilarApp.jsx
+++ client/src/components/SimilarApp/SimilarApp.jsx
@@ -3,14 +3,17 @@ import Carousel from '../Carousel';
 import styles from './SimilarApp.css';
 
 const SimilarApp = props => (
-  <div className={ styles.box }>
-    <div className={ styles.titleWrapper }>
-      <div className={ styles.title }>
-        Similar Products
+  <>
+    <div className={ styles.divider }></div>
+    <div className={ styles.box }>
+      <div className={ styles.titleWrapper }>
+        <div className={ styles.title }>
+          Similar Products
+        </div>
       </div>
+      <Carousel id={ props.id }/>
     </div>
-    <Carousel id={ props.id }/>
-  </div>
+  </>
 );
 
 export default SimilarApp;
