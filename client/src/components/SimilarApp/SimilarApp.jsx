2c0968cb002e9413f92c4a81d0148f977c28d08d
Mina Sorsok <mina.sorsok@gmail.com>
2019-02-23 16:02:11 -0800

    Remove divider

--- client/src/components/SimilarApp/SimilarApp.jsx
+++ client/src/components/SimilarApp/SimilarApp.jsx
@@ -4,14 +4,11 @@ import styles from './SimilarApp.css';
 
 const SimilarApp = props => (
   <>
-    <div className={ styles.divider }></div>
-    <div className={ styles.box }>
-      <div className={ styles.titleWrapper }>
-        <div className={ styles.title }>
-          Similar Products
-        </div>
+    <div className={styles.box}>
+      <div className={styles.titleWrapper}>
+        <div className={styles.title}>Similar Products</div>
       </div>
-      <Carousel id={ props.id }/>
+      <Carousel id={props.id} />
     </div>
   </>
 );
