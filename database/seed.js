3a854b79f0096ddb02dcb40058862416e78b5968
Jun Kelly <kellyja4@gmail.com>
2019-02-25 23:39:52 -0800

    Removed comment

--- database/seed.js
+++ database/seed.js
@@ -96,28 +96,3 @@ LikeList.find().then(result => {
     saveLike(generateLikeList(testData));
   }
 });
-
-// function save(data) {
-//   data.forEach(entry => {
-//     entry = new SimilarList({
-//       product_name: entry.product_name,
-//       category: entry.category,
-//       size: randomNumberArr(randomNumberInt(1, 5)),
-//       description: entry.description,
-//       sku: randomNumberInt(1000000, 2000000),
-//       stars: randomNumberDec(0, 5),
-//       reviews: entry.reviews,
-//       badge: Math.random() >= 0.9,
-//       loves: entry.loves,
-//       exclusive: Math.random() >= 0.7,
-//       online_only: Math.random() >= 0.7,
-//       limited_edition: Math.random() >= 0.8,
-//       free_shipping: Math.random() >= 0.7,
-//       price: `$${randomNumberDec(1, 200)}`,
-//       image: faker.image.imageUrl(300,300,"animals")
-//     }).save()
-//       .catch(err => console.log(err));
-//   })
-// }
-
-// save(newMock);
